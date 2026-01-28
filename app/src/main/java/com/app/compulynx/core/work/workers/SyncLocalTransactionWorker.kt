c373eed8d17ce8d2bc49e7b1f208eb2bba0a52a3
Sam <sammuigai880@gmail.com>
2026-01-28 08:37:39 +0300

    set foreground info (required for workManager)

--- app/src/main/java/com/app/compulynx/core/work/workers/SyncLocalTransactionWorker.kt
+++ app/src/main/java/com/app/compulynx/core/work/workers/SyncLocalTransactionWorker.kt
@@ -1,9 +1,15 @@
 package com.app.compulynx.core.work.workers
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.pm.ServiceInfo
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.Constraints
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.OutOfQuotaPolicy
@@ -20,6 +26,7 @@ class SyncLocalTransactionWorker @AssistedInject constructor(
     private val transactionRepository: TransactionRepository
 ) : CoroutineWorker(appContext, params) {
     override suspend fun doWork(): Result {
+        setForeground(getForegroundInfo())
         return try {
             transactionRepository.syncLocalTransactions()
             Result.success()
@@ -29,6 +36,46 @@ class SyncLocalTransactionWorker @AssistedInject constructor(
         } catch (e: Exception) {
             e.printStackTrace()
             return Result.failure()
+        }
+    }
+
+    override suspend fun getForegroundInfo(): ForegroundInfo {
+        val notificationId = 1
+        val channelId = "sync_channel"
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Background Sync",
+                NotificationManager.IMPORTANCE_MIN,
+            ).apply {
+                setSound(null, null)
+                enableVibration(false)
+                setShowBadge(false)
+            }
+
+            val manager =
+                applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+
+        val notification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(android.R.drawable.stat_sys_upload)
+            .setContentTitle("Syncing transactions")
+            .setContentText("Syncing local transactions...")
+            .setPriority(NotificationCompat.PRIORITY_MIN)
+            .setOngoing(true)
+            .setSilent(true)
+            .build()
+
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ForegroundInfo(
+                notificationId,
+                notification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC,
+            )
+        } else {
+            ForegroundInfo(notificationId, notification)
         }
     }
 
